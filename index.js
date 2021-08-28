b644ae6d3312b9aa24592a94bfa70d18260cf3c6
Mohnish Thallavajhula <hi@iam.mt>
2021-08-28 12:46:38 -0700

    Setup all the commands

--- index.js
+++ index.js
@@ -24,6 +24,38 @@ bot.command('sup', (ctx) => {
   ctx.reply(`<3 @${ctx.update.message.from.username}`);
 });
 
+bot.command('active', (ctx) => {
+  // Respond with list of active pots
+  ctx.reply('response to active');
+});
+
+bot.command('past', (ctx) => {
+  // Respond with list of past pots with a date limit
+  ctx.reply('response to past');
+});
+
+bot.command('upcoming', (ctx) => {
+  // Respond with list of upcoming pots
+  ctx.reply('response to upcoming');
+});
+
+bot.command('start', (ctx) => {
+  // DB: Create new Pot with the params
+  // Respond with custom keyboard
+  ctx.reply('response to start');
+});
+
+bot.command('join', (ctx) => {
+  // DB: Update outcome array of Pot with the ID
+  const [_, id, outcome] = ctx.update.message.text.split(' ');
+
+  ctx.reply(`You're joining ${id} and rooting for ${outcome}`);
+});
+
+bot.command('info', (ctx) => {
+  ctx.reply('response to info');
+});
+
 // bot.on('callback_query', (ctx) => {
 //   // Explicit usage
 //   ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
