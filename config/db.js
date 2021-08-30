2042fefa2f0a772c313b8fc9add7e9a91e443631
Mohnish Thallavajhula <hi@iam.mt>
2021-08-29 19:10:42 -0700

    Switch DB URL const and change indentation

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 import mongooseConnection from 'mongoose';
-mongooseConnection.connect(process.env.MONGO_SRV)
+mongooseConnection.connect(process.env.DB_CONN_URL)
 export default mongooseConnection;
--- models/pot.js
+++ models/pot.js
@@ -1,42 +1,38 @@
 import mongoose from '../config/db.js';
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 export default mongoose.model("Pot", mongoose.Schema({
-    potId: {
-        type: String,
-        required: true
-    },
-    event: {
-        type: String,
-        required: true
-    },
-    outcomes:{
-        type: Schema.Types.Mixed,
-        required: true
-    },
-    buyIn: {
-        type: String,
-        required: true
-    },
-    startAt: {
-        type: Date,
-        required: true
-    },
-    stopAt: {
-        type: Date,
-        required: true
-    },
-    status: {
-        type: String,
-        required: true
-    },
-    creatorId: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        required: true
-    }
+  event: {
+    type: String,
+    required: true
+  },
+  outcomes:{
+    type: Schema.Types.Mixed,
+    required: true
+  },
+  buyIn: {
+    type: String,
+    required: true
+  },
+  startAt: {
+    type: Date,
+    required: true
+  },
+  stopAt: {
+    type: Date,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  creator: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true
+  }
 }));
 
